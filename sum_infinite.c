4582fedd6c0c531384e76c2f2d4d56ab4081e116
BasUitermark <basuitermark@outlook.com>
2023-04-01 16:37:03 +0200

    just need to remove 0's from the end of the list

--- sum_infinite.c
+++ sum_infinite.c
@@ -85,48 +85,77 @@ void sum_infinite(t_list **data, char *str1, char *str2, bool neg)
 		add_front(data, lstnew('-'));
 }
 
+void order_strings(char **str1, char **str2)
+{
+	bool	s1_bigger = true;
+
+	int		len1 = ft_strlen(*str1);
+	int		len2 = ft_strlen(*str2);
+	len1 = *str1[0] == '-' ? len1 += -1 : len1;
+	len2 = *str2[0] == '-' ? len2 += -1 : len2;
+
+	if (len1 > len2)
+		swap(str1, str2);
+	else if (len2 > len1)
+		return ;
+	else
+	{
+		len1 = ft_strlen(*str1);
+		len2 = ft_strlen(*str2);
+		int i = *str1[0] == '-' ? 1 : 0;
+		int j = *str2[0] == '-' ? 1 : 0;
+		char	*str1t = *str1;
+		char	*str2t = *str2;
+		while (i < len1 && j < len2)
+		{
+			if (str1t[i] < str2t[j])
+			{
+				s1_bigger = false;
+				break;
+			}
+			else if (str2t[j] < str1t[i])
+			{
+				s1_bigger = true;
+				break;
+			}
+			i++;
+			j++;
+		}
+		if (s1_bigger)
+			swap(str1, str2);
+		else if (len2 > len1)
+			return ;
+	}
+}
+
 void sub_infinite(t_list **data, char *str1, char *str2)
 {
+	order_strings(&str1, &str2);
+	bool large_neg = str2[0] == '-' ? true : false;
+
+	int len1 = ft_strlen(str2) - 1;
+	int len2 = ft_strlen(str1) - 1;
+	int carry = 0;
 	
-	// int	n;
-	// int	carry = 0;
-	// int	dif;
-	// int	len1 = ft_strlen(str1);
-	// int len2 = ft_strlen(str2);
-
-
-	// if (len1 > len2)
-	// 	swap(&str1, &str2);
-	// len1 = ft_strlen(str1);
-	// len2 = ft_strlen(str2);
-	// dif = len2 - len1;
-
-	// for (int i = len1 - 1; i >= 0 && str1[i] != '-'; i--)
-	// {
-	// 	n = (str1[i] - '0') - (str2[i + dif] - '0') - carry;
-	// 	if (n < 0)
-	// 	{
-	// 		n += 10;
-	// 		carry = n / 10;
-	// 	}
-	// 	else
-	// 		carry = 0;
-	// 	add_front(data, lstnew(n % 10 + '0'));
-	// }
-	// for (int i = len2 - len1 - 1; i >= 0 && str2[i] != '-'; i--)
-	// {
-	// 	n = (str2[i] - '0') - carry;
-	// 	if (n < 0)
-	// 	{
-	// 		n += 10;
-	// 		carry = n / 10;
-	// 	}
-	// 	else
-	// 		carry = 0;
-	// 	add_front(data, lstnew(n % 10 + '0'));
-	// }
-	// // if (carry)
-	// // 	add_front(data, lstnew(carry + '0'));
+	while (len1 >= 0 || len2 >= 0) 
+	{
+		if (str2[len1] == '-' || str1[len2] == '-')
+			break ;
+		int num1 = (len1 >= 0) ? (str2[len1--] - '0') : 0;
+		int num2 = (len2 >= 0) ? (str1[len2--] - '0') : 0;
+		int sub = num1 - num2 - carry;
+		if (sub < 0) 
+		{
+			sub += 10;
+			carry = 1;
+		}
+		else
+			carry = 0;
+		if (len1 >= 0 && carry)
+			add_front(data, lstnew(sub % 10 + '0'));
+	}
+	if (large_neg)
+		add_front(data, lstnew('-'));
 }
 
 void	determine_type(t_list **data, char *str1, char *str2)
@@ -152,7 +181,6 @@ int main(int argc, char *argv[])
 	t_list	*data = NULL;
 	if (argc != 3)
 		return (1);
-
 	determine_type(&data, argv[1], argv[2]);
 	while (data)
 	{
