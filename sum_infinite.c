3c388a0bfd060fe5ae015f041304af3ca5d97728
BasUitermark <basuitermark@outlook.com>
2023-04-01 13:59:57 +0200

    added non functioning substract function :(

--- sum_infinite.c
+++ sum_infinite.c
@@ -14,7 +14,7 @@ t_list	*lstnew(char data)
 	t_list	*head;
 
 	head = (t_list *)malloc(sizeof(t_list));
-	if (head == NULL)
+	if (!head)
 		return (0);
 	head->data = data;
 	head->next = NULL;
@@ -50,32 +50,101 @@ void swap(char **str1, char **str2)
 	*str2 = temp;
 }
 
-void	sum_infinite(t_list **data, char *str1, char *str2)
+void sum_infinite(t_list **data, char *str1, char *str2, bool neg)
 {
-	int		n;
-	int		carry = 0;
-	int		len1 = ft_strlen(str1);
-	int 	len2 = ft_strlen(str2);
+	int	n;
+	int	carry = 0;
+	int	dif;
+	int	len1 = ft_strlen(str1);
+	int len2 = ft_strlen(str2);
+
 
 	if (len1 > len2)
 		swap(&str1, &str2);
 	len1 = ft_strlen(str1);
 	len2 = ft_strlen(str2);
-	for (int i = len1 - 1; i >= 0; i--)
+	dif = len2 - len1;
+
+	for (int i = len1 - 1; i >= 0 && str1[i] != '-'; i--)
 	{
-		n = (str1[i] - '0') + (str2[i + (len2 - len1)] - '0') + carry;
-		carry += n / 10;
+		n = (str1[i] - '0') + (str2[i + dif] - '0') + carry;
+		carry = n / 10;
 		add_front(data, lstnew(n % 10 + '0'));
 	}
-	for (int i = len2 - len1 -1; i >= 0; i--)
+	if (neg)
+		len2++;
+	for (int i = len2 - len1 - 1; i >= 0 && str2[i] != '-'; i--)
 	{
-		n = str2[i] - '0' + carry;
-		carry += n / 10;
+		n = (str2[i] - '0') + carry;
+		carry = n / 10;
 		add_front(data, lstnew(n % 10 + '0'));
 	}
 	if (carry)
 		add_front(data, lstnew(carry + '0'));
+	if (neg)
+		add_front(data, lstnew('-'));
+}
 
+void sub_infinite(t_list **data, char *str1, char *str2)
+{
+	
+	// int	n;
+	// int	carry = 0;
+	// int	dif;
+	// int	len1 = ft_strlen(str1);
+	// int len2 = ft_strlen(str2);
+
+
+	// if (len1 > len2)
+	// 	swap(&str1, &str2);
+	// len1 = ft_strlen(str1);
+	// len2 = ft_strlen(str2);
+	// dif = len2 - len1;
+
+	// for (int i = len1 - 1; i >= 0 && str1[i] != '-'; i--)
+	// {
+	// 	n = (str1[i] - '0') - (str2[i + dif] - '0') - carry;
+	// 	if (n < 0)
+	// 	{
+	// 		n += 10;
+	// 		carry = n / 10;
+	// 	}
+	// 	else
+	// 		carry = 0;
+	// 	add_front(data, lstnew(n % 10 + '0'));
+	// }
+	// for (int i = len2 - len1 - 1; i >= 0 && str2[i] != '-'; i--)
+	// {
+	// 	n = (str2[i] - '0') - carry;
+	// 	if (n < 0)
+	// 	{
+	// 		n += 10;
+	// 		carry = n / 10;
+	// 	}
+	// 	else
+	// 		carry = 0;
+	// 	add_front(data, lstnew(n % 10 + '0'));
+	// }
+	// // if (carry)
+	// // 	add_front(data, lstnew(carry + '0'));
+}
+
+void	determine_type(t_list **data, char *str1, char *str2)
+{
+	bool	neg1 = false;
+	bool	neg2 = false;
+
+	if (str1[0] == '-')
+		neg1 = true;
+	if (str2[0] == '-')
+		neg2 = true;
+	
+	if (!neg1 && !neg2)
+		sum_infinite(data, str1, str2, false);
+	else if (neg1 && neg2)
+		sum_infinite(data, str1, str2, true);
+	else
+		sub_infinite(data, str1, str2);
 }
 
 int main(int argc, char *argv[])
@@ -84,7 +153,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		return (1);
 
-	sum_infinite(&data, argv[1], argv[2]);
+	determine_type(&data, argv[1], argv[2]);
 	while (data)
 	{
 		write(1, &data->data, 1);
