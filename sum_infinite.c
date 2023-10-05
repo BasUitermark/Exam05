626b7c7436875907e27e83d7dd579fd492970aee
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-10-05 23:39:21 +0200

    Update sum_infinite.c

--- sum_infinite.c
+++ sum_infinite.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -11,195 +10,100 @@ typedef struct s_list
 
 t_list	*lstnew(char data)
 {
-	t_list	*head;
-
-	head = (t_list *)malloc(sizeof(t_list));
-	if (!head)
-		return (0);
-	head->data = data;
-	head->next = NULL;
-	return (head);
+	t_list	*node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->data = data;
+	node->next = NULL;
+	return (node);
 }
 
-void	add_front(t_list **lst, t_list *new_item)
+void	add_front(t_list **lst, char data)
 {
-	if (!lst)
-	{
-		*lst = new_item;
-		return ;
-	}
-	new_item->next = *lst;
-	*lst = new_item;
-}
-
-void	delete_first(t_list **head)
-{
-	t_list	*tmp;
-
-	if (head == NULL || *head == NULL)
-		return ;
-
-	tmp = *head;
-	*head = (*head)->next;
-	free(tmp);
+	t_list	*node = lstnew(data);
+	if (!node)
+		return;
+	node->next = *lst;
+	*lst = node;
 }
 
 int	ft_strlen(char *str)
 {
-	int	i;
-	if (!str)
-		return (0);
-
-	for (i = 0; str[i]; i++){
-	}
+	int	i = 0;
+	while (str[i])
+		i++;
 	return (i);
 }
 
-void	swap(char **str1, char **str2)
+void	sum_infinite(t_list **result, char *str1, char *str2)
 {
-	char *temp = *str1;
-	*str1 = *str2;
-	*str2 = temp;
-}
-
-void	sum_infinite(t_list **data, char *str1, char *str2, bool neg)
-{
-	int	n;
-	int	carry = 0;
-	int	dif;
 	int	len1 = ft_strlen(str1);
 	int	len2 = ft_strlen(str2);
-
-
-	if (len1 > len2)
-		swap(&str1, &str2);
-	len1 = ft_strlen(str1);
-	len2 = ft_strlen(str2);
-	dif = len2 - len1;
-
-	for (int i = len1 - 1; i >= 0 && str1[i] != '-'; i--)
-	{
-		n = (str1[i] - '0') + (str2[i + dif] - '0') + carry;
-		carry = n / 10;
-		add_front(data, lstnew(n % 10 + '0'));
-	}
-	if (neg)
-		len2++;
-	for (int i = len2 - len1 - 1; i >= 0 && str2[i] != '-'; i--)
-	{
-		n = (str2[i] - '0') + carry;
-		carry = n / 10;
-		add_front(data, lstnew(n % 10 + '0'));
-	}
-	if (carry)
-		add_front(data, lstnew(carry + '0'));
-	if (neg)
-		add_front(data, lstnew('-'));
-}
-
-void	order_strings(char **str1, char **str2)
-{
-	bool	s1_bigger = true;
-
-	int		len1 = ft_strlen(*str1);
-	int		len2 = ft_strlen(*str2);
-	len1 = *str1[0] == '-' ? len1 += -1 : len1;
-	len2 = *str2[0] == '-' ? len2 += -1 : len2;
-
-	if (len1 > len2)
-		swap(str1, str2);
-	else if (len2 > len1)
-		return ;
-	else
-	{
-		len1 = ft_strlen(*str1);
-		len2 = ft_strlen(*str2);
-		int i = *str1[0] == '-' ? 1 : 0;
-		int j = *str2[0] == '-' ? 1 : 0;
-		char	*str1t = *str1;
-		char	*str2t = *str2;
-		while (i < len1 && j < len2)
-		{
-			if (str1t[i] < str2t[j])
-			{
-				s1_bigger = false;
-				break ;
-			}
-			else if (str2t[j] < str1t[i])
-			{
-				s1_bigger = true;
-				break ;
-			}
-			i++;
-			j++;
-		}
-		if (s1_bigger)
-			swap(str1, str2);
-		else if (len2 > len1)
-			return ;
-	}
-}
-
-void	sub_infinite(t_list **data, char *str1, char *str2)
-{
-	order_strings(&str1, &str2);
-	bool large_neg = str2[0] == '-' ? true : false;
-
-	int	len1 = ft_strlen(str2) - 1;
-	int	len2 = ft_strlen(str1) - 1;
 	int	carry = 0;
 
-	while (len1 >= 0 || len2 >= 0)
+	while (len1 > 0 || len2 > 0 || carry)
 	{
-		if (str2[len1] == '-' || str1[len2] == '-')
-			break ;
-		int num1 = (len1 >= 0) ? (str2[len1--] - '0') : 0;
-		int num2 = (len2 >= 0) ? (str1[len2--] - '0') : 0;
-		int sub = num1 - num2 - carry;
-		if (sub < 0)
+		int num1 = (len1-- > 0) ? (str1[len1] - '0') : 0;
+		int num2 = (len2-- > 0) ? (str2[len2] - '0') : 0;
+		int sum = num1 + num2 + carry;
+
+		add_front(result, (sum % 10) + '0');
+		carry = sum / 10;
+	}
+}
+
+void	sub_infinite(t_list **result, char *str1, char *str2)
+{
+	int len1 = ft_strlen(str1);
+	int len2 = ft_strlen(str2);
+	int carry = 0;
+
+	while (len1 > 0 || len2 > 0)
+	{
+		int num1 = (len1-- > 0) ? (str1[len1] - '0') : 0;
+		int num2 = (len2-- > 0) ? (str2[len2] - '0') : 0;
+		int diff = num1 - num2 - carry;
+
+		if (diff < 0)
 		{
-			sub += 10;
+			diff += 10;
 			carry = 1;
 		}
 		else
 			carry = 0;
-		add_front(data, lstnew(sub % 10 + '0'));
+
+		add_front(result, (diff % 10) + '0');
 	}
-	while ((*data)->data == '0' && (*data)->next)
-		delete_first(data);
-	if (large_neg && (*data)->data != '0')
-		add_front(data, lstnew('-'));
-}
-
-void	determine_type(t_list **data, char *str1, char *str2)
-{
-	bool	neg1 = false;
-	bool	neg2 = false;
-
-	if (str1[0] == '-')
-		neg1 = true;
-	if (str2[0] == '-')
-		neg2 = true;
-
-	if (!neg1 && !neg2)
-		sum_infinite(data, str1, str2, false);
-	else if (neg1 && neg2)
-		sum_infinite(data, str1, str2, true);
-	else
-		sub_infinite(data, str1, str2);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_list	*data = NULL;
 	if (argc != 3)
 		return (1);
-	determine_type(&data, argv[1], argv[2]);
-	while (data)
+
+	t_list	*result = NULL;
+	bool is_neg1 = (argv[1][0] == '-');
+	bool is_neg2 = (argv[2][0] == '-');
+
+	if (is_neg1)
+		argv[1]++;
+	if (is_neg2)
+		argv[2]++;
+
+	if (is_neg1 == is_neg2)
+		sum_infinite(&result, argv[1], argv[2]);
+	else
+		sub_infinite(&result, argv[1], argv[2]);
+
+	if ((is_neg1 && !is_neg2 && ft_strlen(argv[1]) > ft_strlen(argv[2])) || (is_neg2 && !is_neg1 && ft_strlen(argv[2]) > ft_strlen(argv[1])))
+		write(1, "-", 1);
+
+	while (result)
 	{
-		write(1, &data->data, 1);
-		data = data->next;
+		write(1, &result->data, 1);
+		result = result->next;
 	}
 	write(1, "\n", 1);
+
 	return (0);
 }
